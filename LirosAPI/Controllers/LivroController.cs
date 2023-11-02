7e5928bf4ee607444d4c63937f3c194ba9c56279
thal01veloso <thal01veloso@gmail.com>
2023-11-02 14:35:13 -0300

    muda o diretório

---
+++ LirosAPI/Controllers/LivroController.cs
@@ -0,0 +1,38 @@
+using livrariaApi.context;
+using livrariaApi.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace livrariaApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LivroController : ControllerBase
+{
+    private readonly LivroDbContext _context;
+
+    public LivroController(LivroDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public IEnumerable<Livro> GetClientes()
+    {
+        return  _context.livros.ToList();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateLivros([FromBody] Livro livro)
+    {
+        try
+        {
+            _context.livros.Add(livro);
+            var response = await _context.SaveChangesAsync();
+            return Ok("Inserido Com Sucesso !!!");
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+    }
+}
---
+++ LirosAPI/readme.md
@@ -0,0 +1,28 @@
+Rodar o programa
+- dotnet run
+Migração banco de dados
+- dotnet ef migrations add NomeDaNovaMigracao
+- dotnet ef databa
+- se update
+EXEMPLO DE REQUISIÇÃO:
+- {
+  "editora": "Editora Exemplo",
+  "edicao": 1,
+  "anoPublicado": "2023",
+  "livroAssuntos": [
+    {
+      "assuntoId": 1,
+      "assunto": {
+        "descricao": "Ficção Científica"
+      }
+    }
+  ],
+  "livroAutores": [
+    {
+      "autorId": 1,
+      "autor": {
+        "nome": "John Doe"
+      }
+    }
+  ]
+}
